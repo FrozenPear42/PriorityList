28e2a8628463910ca5f2c4ea634990bb6a52554c
Wojciech Gruszka <wojciech@gruszka.eu>
2016-03-06 18:44:08 +0100

    [FIX] Improved testing API

--- inc/Test.hpp
+++ inc/Test.hpp
@@ -7,12 +7,22 @@
 #include <functional>
 #include <iostream>
 
-#define assert(expr) try{ \
-if(!(expr)){self.out << "Test " << self.mName << " failed at: " #expr "\n"; return false;} \
-else {self.out << "[OK] " #expr << "\n";} \
-}catch(...){self.out << "Test " << self.mName << " failed at: " #expr "\n" << "Exception: " << "happened!" << "\n"; return false;}
+#define COLOR_RED "\x1B[31m"
+#define COLOR_GREEN "\x1B[32m"
+#define COLOR_YELLOW "\x1B[33m"
+#define COLOR_BLUE "\x1B[34m"
+#define COLOR_RESET "\x1B[0m"
 
-#define eval(expr) self.out <<"[OP] " #expr "\n"; expr;
+
+#define assert(expr) try{ \
+if(!(expr)){self.out << "[" COLOR_RED "FAIL" COLOR_RESET "] " #expr << "\n";; return false;} \
+else {self.out << "[" COLOR_GREEN "OK" COLOR_RESET "]   " #expr << "\n";} \
+}catch(...){self.out << "[" COLOR_RED "FAIL" COLOR_RESET "] " #expr << " EXCEPTION" << "\n"; return false;}
+
+#define eval(expr) self.out <<"[" COLOR_YELLOW "EXP" COLOR_RESET "]  " #expr "\n"; expr;
+
+#define log(comment, data) self.out << "[" COLOR_BLUE "LOG" COLOR_RESET "]  " << comment << ": " << data << "\n";
+#define log_obj(obj) self.out << "[" COLOR_BLUE "LOG" COLOR_RESET "]  " << #obj ": " << obj << "\n";
 
 class Test {
 public:
@@ -20,9 +30,9 @@ public:
         out << "Running test " << mName << ":\n";
         bool res = mExpression(*this);
         if(res)
-        out << "Success!\n";
+        out << COLOR_GREEN "Success!\n" COLOR_RESET;
         else
-        out << "Fail!\n";
+        out << COLOR_RED "Fail!\n" COLOR_RESET;
         out << "\n";
         return res;
     };
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, const char **argv)
 
     tests.push_back(Test("initialization list", [](Test& self){
         PriorityList list = {1, 2, 3, 4, 5};
-        self.out << "list: " << list << '\n';
+        log_obj(list);
         assert(list.length() == 5);
         assert(list.find(1) != -1);
         assert(list.find(2) != -1);
@@ -21,16 +21,16 @@ int main(int argc, const char **argv)
 
     tests.push_back(Test("length", [](Test& self){
         PriorityList list = {-15555, 0, 1222};
-        self.out << "list: " << list << '\n';
+        log_obj(list);
         assert(list.length() == 3);
         eval(list.pushBack(11));
-        self.out << "list: " << list << '\n';
+        log_obj(list);
         assert(list.length() == 4);
         eval(list.removeOneByValue(0));
-        self.out << "list: " << list << '\n';
+        log_obj(list);
         assert(list.length() == 3);
         eval(list.removeAll());
-        self.out << "list: " << list << '\n';
+        log_obj(list);
         assert(list.length() == 0);
         return true;
     }));
@@ -38,8 +38,8 @@ int main(int argc, const char **argv)
     tests.push_back(Test("copy constructor", [](Test& self){
         PriorityList list1 = {1, 2, 3, 4, 5};
         PriorityList list2(list1);
-        self.out << "list1: " << list1 << '\n';
-        self.out << "list2: " << list2 << '\n';
+        log_obj(list1);
+        log_obj(list2);
         assert(list1 == list2);
         return true;
     }));
@@ -47,68 +47,28 @@ int main(int argc, const char **argv)
     tests.push_back(Test("assigment operator", [](Test& self){
         PriorityList list1 = {1, 2, 3, 4, 5};
         PriorityList list2;
-        self.out << "list1: " << list1 << '\n';
-        self.out << "list2: " << list2 << '\n';
+        log_obj(list1);
+        log_obj(list2);
         eval(list2 = list1);
-        self.out << "list1: " << list1 << '\n';
-        self.out << "list2: " << list2 << '\n';
+        log_obj(list1);
+        log_obj(list2);
         assert(list1 == list2);
         return true;
     }));
 
     tests.push_back(Test("remove duplicates", [](Test& self){
         PriorityList list = {1, 1, 2, 3, 55, -11, 5, -11, 2, 1, 1, 3, 2, 11};
-        self.out << "list: " << list << '\n';
+        log_obj(list);
         assert(list.length() == 14);
         eval(list.removeDuplicates());
-        self.out << "list: " << list << '\n';
+        log_obj(list);
         assert(list.length() == 7);
         return true;
     }));
 
-
-
     for(Test& test : tests)
         test.run();
-    /*
-    PriorityList list;
 
-    list.pushFront(222);
-    list.pushFront(255);
-    list.pushFront(314);
-    list.pushFront(314);
-    list.pushBack(314);
-    list.pushBack(222);
-    list.pushBack(255);
-    cout << list << endl;
-    PriorityList list2(list);
-    list.removeDuplicates();
-    cout << "List1:\n" << list << endl;
-    cout << "List2:\n" << list2 << endl;
-    PriorityList a =  list2 + list;
-    cout << "List2 + List1 =\n" << a << endl;
-    a = list2 - list;
-    cout << "List2 - List1 =\n" << a << endl;
-    cout << "List2 == List1 " << (list2==list) << endl;
-    cout << "List1 == List1 " << (list==list) << endl;
 
-    //cout << list.length() << " " << list[0] << endl;
-    //cout << "find(271): " << list.find(271) << endl;
-    //list.pushFront(271);
-    //cout << list.length() << " " << list[0] << endl;
-    //cout << list << endl;
-    //cout << list.length() << " " << list[2] << endl;
-    //cout << list << endl;
-    //cout << "find(222): " <<   list.find(222) << endl;
-    //cout << "find(222): " <<   list.find(222) << endl;
-    //cout << "find(222): " <<   list.find(222) << endl;
-    //cout << list << endl;
-    //list.removeAllByValue(314);
-    //cout << "Remove: " << list.length() << endl;
-    //cout << "find(271): " << list.find(271) << endl;
-    //cout << "Removing dups:\n";
-    //list.removeDuplicates();
-    //cout << list << endl;
-    */
     return 0;
 }
